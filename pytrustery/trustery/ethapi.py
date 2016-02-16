2f1aafd5ca8face954463fe82104289c43fb33c8
Mustafa <mus@musalbas.com>
2016-02-16 12:49:43 +0000

    Switch to ethereum-rpc-client

--- pytrustery/trustery/ethapi.py
+++ pytrustery/trustery/ethapi.py
@@ -9,7 +9,7 @@ import trustery
 TRUSTERY_DEFAULT_ADDRESS = '0xd7f4a7b264ff1e5d25d12566c60ec726872a8a09'
 TRUSTERY_ABI = json.load(open(os.path.join(os.path.dirname(trustery.__file__), 'trustery_abi.json')))
 
-ethclient = eth_rpc_client.Client('http://127.0.0.1:8545')
+ethclient = eth_rpc_client.Client(host='127.0.0.1', port='8545')
 
 
 def encode_api_data(data):

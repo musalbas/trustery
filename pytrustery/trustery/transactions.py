718bc13be49dfba9f3775477821277e800a06be7
Mustafa <mus@musalbas.com>
2016-02-20 14:47:01 +0000

    Add API for signing attributes and revoking signatures.

--- pytrustery/trustery/transactions.py
+++ pytrustery/trustery/transactions.py
@@ -53,3 +53,24 @@ class Transactions(object):
         args = [attributetype, has_proof, identifier, data, datahash]
         data = self._contracttranslator.encode('addAttribute', args)
         return self._send_transaction(data)
+
+    def sign_attribute(self, attributeID, expiry):
+        """
+        Send a transaction to sign an identity attriute.
+
+        attributeID: the ID of the attribute.
+        expiry: the expiry time of the attriute.
+        """
+        args = [attributeID, expiry]
+        data = self._contracttranslator.encode('signAttribute', args)
+        return self._send_transaction(data)
+
+    def revoke_signature(self, signatureID):
+        """
+        Send a transaction to revoke a signature.
+
+        signatureID: the ID of the signature.
+        """
+        args = [signatureID]
+        data = self._contracttranslator.encode('revokeSignature', args)
+        return self._send_transaction(data)

ffa6c1b5773a9eaf1644daabfdb7e327d27ee741
Mustafa <mus@musalbas.com>
2016-03-01 14:40:18 +0000

    Prettify JSON

--- build-contract.sh
+++ build-contract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-solc contract/trustery.sol --abi | tail -2 > pytrustery/trustery/trustery_abi.json
+solc contract/trustery.sol --abi | tail -2 | python -m json.tool > pytrustery/trustery/trustery_abi.json

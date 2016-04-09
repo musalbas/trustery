3cda5fb895e833b588fc3c3bf0c41bd31403f3c6
Mustafa <mus@musalbas.com>
2016-04-10 00:30:31 +0100

    trusted->is_trusted

--- pytrustery/test/userconfig.py
+++ pytrustery/test/userconfig.py
@@ -5,13 +5,13 @@ from trustery.userconfig import *
 
 class TestUserConfig(unittest.TestCase):
     def test_truststore(self):
-        self.assertFalse(trusted('foo'))
+        self.assertFalse(is_trusted('foo'))
 
         trust('foo')
-        self.assertTrue(trusted('foo'))
+        self.assertTrue(is_trusted('foo'))
 
         untrust('foo')
-        self.assertFalse(trusted('foo'))
+        self.assertFalse(is_trusted('foo'))
 
 if __name__ == '__main__':
     unittest.main()

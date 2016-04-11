c60f14f7229f716bbd9946331acdd5904dbe6729
Mustafa <mus@musalbas.com>
2016-04-11 19:25:01 +0100

    Add PGP unit tsts.

---
+++ pytrustery/test/pgp.py
@@ -0,0 +1,24 @@
+import pickle
+import unittest
+
+from trustery.events import Events
+
+
+class TestPGP(unittest.TestCase):
+    def test_verify_attribute_pgp_proof(self):
+        events = Events()
+
+        attribute = pickle.load(open('pgp_attribute_1.pickle'))
+        valid = events.verify_attribute_pgp_proof(attribute)
+        self.assertTrue(valid)
+
+        attribute = pickle.load(open('pgp_attribute_2.pickle'))
+        valid = events.verify_attribute_pgp_proof(attribute)
+        self.assertIsNone(valid)
+
+        attribute = pickle.load(open('pgp_attribute_3.pickle'))
+        valid = events.verify_attribute_pgp_proof(attribute)
+        self.assertFalse(valid)
+
+if __name__ == '__main__':
+    unittest.main()

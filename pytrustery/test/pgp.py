efdc875e909f501d26b25fe4f62301ff14a14426
Mustafa <mus@musalbas.com>
2016-04-11 19:29:11 +0100

    move pickle files to res folder

--- pytrustery/test/pgp.py
+++ pytrustery/test/pgp.py
@@ -8,15 +8,15 @@ class TestPGP(unittest.TestCase):
     def test_verify_attribute_pgp_proof(self):
         events = Events()
 
-        attribute = pickle.load(open('pgp_attribute_1.pickle'))
+        attribute = pickle.load(open('res/pgp_attribute_1.pickle'))
         valid = events.verify_attribute_pgp_proof(attribute)
         self.assertTrue(valid)
 
-        attribute = pickle.load(open('pgp_attribute_2.pickle'))
+        attribute = pickle.load(open('res/pgp_attribute_2.pickle'))
         valid = events.verify_attribute_pgp_proof(attribute)
         self.assertIsNone(valid)
 
-        attribute = pickle.load(open('pgp_attribute_3.pickle'))
+        attribute = pickle.load(open('res/pgp_attribute_3.pickle'))
         valid = events.verify_attribute_pgp_proof(attribute)
         self.assertFalse(valid)
 

d06b62b8f77ada93ab90406e40143bc0ed9b0ec4
Mustafa <mus@musalbas.com>
2016-04-20 17:58:55 +0100

    Add build script for alt smart contract.

--- build-contract.sh
+++ build-contract.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-solc contract/trustery.sol --abi | tail -2 | python -m json.tool > pytrustery/trustery/trustery_abi.json
+if [ "$1" == "--alt" ]
+then
+    solc contract/trustery-alt.sol --abi | tail -2 | python -m json.tool > pytrustery/trustery/trustery_abi.json
+else
+    solc contract/trustery.sol --abi | tail -2 | python -m json.tool > pytrustery/trustery/trustery_abi.json
+fi

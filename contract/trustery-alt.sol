229241ea66ef7435d751765a791fe9c033914d16
Mustafa <mus@musalbas.com>
2016-04-20 17:52:36 +0100

    Add alternative Trustey smart contract (minimalistic).

---
+++ contract/trustery-alt.sol
@@ -0,0 +1,32 @@
+contract Trustery {
+    struct Signature {
+        address signer;
+    }
+
+    int public attributes;
+    Signature[] public signatures;
+    int public revocations;
+
+    event AttributeAdded(uint indexed attributeID, address indexed owner, string attributeType, bool has_proof, bytes32 indexed identifier, string data, string datahash);
+    event AttributeSigned(uint indexed signatureID, address indexed signer, uint indexed attributeID, uint expiry);
+    event SignatureRevoked(uint indexed revocationID, uint indexed signatureID);
+
+    function addAttribute(string attributeType, bool has_proof, bytes32 identifier, string data, string datahash) returns (uint attributeID) {
+        attributeID = attributes++;
+        AttributeAdded(attributeID, msg.sender, attributeType, has_proof, identifier, data, datahash);
+    }
+
+    function signAttribute(uint attributeID, uint expiry) returns (uint signatureID) {
+        signatureID = signatures.length++;
+        Signature signature = signatures[signatureID];
+        signature.signer = msg.sender;
+        AttributeSigned(signatureID, msg.sender, attributeID, expiry);
+    }
+
+    function revokeSignature(uint signatureID) returns (uint revocationID) {
+        if (signatures[signatureID].signer == msg.sender) {
+            revocationID = revocations++;
+            SignatureRevoked(revocationID, signatureID);
+        }
+    }
+}

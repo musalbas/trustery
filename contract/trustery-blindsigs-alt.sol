0f43dc967dc536b603795080ae074b3e4e34beca
Mustafa Al-Bassam <mus@musalbas.com>
2016-07-21 11:56:13 +0100

    Add blindsig contract

---
+++ contract/trustery-blindsigs-alt.sol
@@ -0,0 +1,46 @@
+contract Trustery {
+    struct Signature {
+        address signer;
+    }
+
+    uint public attributes;
+    uint public blindedAttributes;
+    Signature[] public signatures;
+    uint public blindSignatures;
+    uint public revocations;
+
+    event AttributeAdded(uint indexed attributeID, address indexed owner, string attributeType, bool has_proof, bytes32 indexed identifier, string data, string datahash);
+    event BlindedAttributeAdded(uint indexed blindedAttributeID, uint indexed signingAttributeID, string data, string datahash);
+    event AttributeSigned(uint indexed signatureID, address indexed signer, uint indexed attributeID, uint expiry);
+    event AttributeBlindSigned(uint indexed blindSignatureID, address indexed signer, string data, string datahash);
+    event SignatureRevoked(uint indexed revocationID, uint indexed signatureID);
+
+    function addAttribute(string attributeType, bool has_proof, bytes32 identifier, string data, string datahash) returns (uint attributeID) {
+        attributeID = attributes++;
+        AttributeAdded(attributeID, msg.sender, attributeType, has_proof, identifier, data, datahash);
+    }
+
+    function addBlindedAttribute(string attributeType, uint signingAttributeID, string data, string datahash) returns (uint blindedAttributeID) {
+        blindedAttributeID = blindedAttributes++;
+        BlindedAttributeAdded(blindedAttributeID, signingAttributeID, data, datahash);
+    }
+
+    function signAttribute(uint attributeID, uint expiry) returns (uint signatureID) {
+        signatureID = signatures.length++;
+        Signature signature = signatures[signatureID];
+        signature.signer = msg.sender;
+        AttributeSigned(signatureID, msg.sender, attributeID, expiry);
+    }
+
+    function signBlindedAttribute(uint attributeID, string data, string datahash) returns (uint blindSignatureID) {
+        blindSignatureID = blindSignatures++;
+        AttributeBlindSigned(blindSignatureID, msg.sender, data, datahash);
+    }
+
+    function revokeSignature(uint signatureID) returns (uint revocationID) {
+        if (signatures[signatureID].signer == msg.sender) {
+            revocationID = revocations++;
+            SignatureRevoked(revocationID, signatureID);
+        }
+    }
+}
